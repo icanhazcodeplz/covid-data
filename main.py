b03bcc0a73f4e6972ee9e9c29570b62f4ce4fa05
icanhazcodeplz <magnusson.brent@gmail.com>
2020-07-19 17:02:10 -0700

    Add ability to search for county and update display

--- main.py
+++ main.py
@@ -4,39 +4,32 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 import cufflinks as cf
-from preprocess_data import preprocess_data
-
-STATE = 'Washington'
-COUNTY = 'Whatcom'
+from preprocess_data import county_list, all_county_data
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-df, table_df = preprocess_data(STATE, COUNTY)
 
-fig = df.iplot(asFigure=True, kind='bar', barmode='group',
-               title='New Cases and Deaths in {} County, {} '.format(COUNTY,
-                                                                     STATE))
-fig.update_layout(autosize=False, width=650, height=350,
-                  margin=dict(l=5, r=5, b=5, t=70, pad=1),
-                  paper_bgcolor="LightSteelBlue",
-                  xaxis=dict(tickformat='%b %d', tickmode='linear',
-                             tick0=df.index[0], dtick=14 * 86400000.0,
-                             showgrid=True, ticks="outside",
-                             tickson="boundaries", ticklen=3, tickangle=45)
-                  )
-# fig.show()
-print()
+def county_fig(county_df, county_key):
+    fig = county_df.iplot(asFigure=True, kind='bar', barmode='group',
+                          title='New Cases and Deaths in {}'.format(county_key))
+    fig.update_layout(autosize=False, width=650, height=350,
+                      margin=dict(l=5, r=5, b=5, t=70, pad=1),
+                      paper_bgcolor="LightSteelBlue",
+                      xaxis=dict(tickformat='%b %d', tickmode='linear',
+                                 tick0=county_df.index[0], dtick=14 * 86400000.0,
+                                 showgrid=True, ticks="outside",
+                                 tickson="boundaries", ticklen=3, tickangle=45)
+                      )
+    # fig.show()
+    return fig
 
 
 def generate_table(dataframe, max_rows=10):
-    print('in gen_table')
     return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
+        html.Thead(html.Tr([html.Th(col) for col in dataframe.columns])),
         html.Tbody([
             html.Tr([
                 html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
@@ -46,21 +39,29 @@ def generate_table(dataframe, max_rows=10):
 
 
 app.layout = html.Div(children=[
-    html.H2(children='Whatcom County Covid-19 Tracker'),
-    generate_table(table_df),
-
-    html.Br(),
-    # html.Div(children='Last update: {}'.format(date_yest)),
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    html.H2(children='Covid-19 Tracker'),
+    html.H6('Search for a County'),
+    dcc.Dropdown(
+        id='county-dropdown',
+        options=county_list(),
+        value=''
+    ),
+    html.Div(id='dd-output'),
 ])
-# app.layout = dash_table.DataTable(
-#     id='table',
-#     columns=[{"name": i, "id": i} for i in df.columns],
-#     data=df.to_dict('records'),
-# )
+
+
+@app.callback(
+    dash.dependencies.Output('dd-output', 'children'),
+    [dash.dependencies.Input('county-dropdown', 'value')])
+def update_output(value):
+    if value:
+        county_df, summary_df = all_county_data(value)
+        fig = county_fig(county_df, value)
+        return html.Div(children=[generate_table(summary_df),
+            dcc.Graph(figure=fig)])
+
+    return ''
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8080)
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,41 +1,37 @@
 import pandas as pd
 import re
 
+#FIXME: Update file locations
+CASES_FILE = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+DEATHS_FILE = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
 
-def counts_by_county(df, state):
-    df_state = df[df['Province_State'] == state].set_index('Admin2')
+# CASES_FILE = 'time_series_covid19_confirmed_US.csv'
+# DEATHS_FILE = 'time_series_covid19_deaths_US.csv'
+
+
+def county_list():
+    cases = pd.read_csv(CASES_FILE)
+    county_keys = cases['Combined_Key'].unique()
+    return [dict(label=k, value=k) for k in county_keys]
+
+
+def county_series(df, county_key):
+    county_s = df[df['Combined_Key'] == county_key].set_index('Admin2')
     date_cols_bool = [bool(re.match('\d*/\d*/\d\d', c)) for c in
-                      df_state.columns]
-    df_state = df_state.iloc[:, date_cols_bool]
-    df_state = df_state.T
-    df_state.index = pd.to_datetime(df_state.index)
-    return df_state
+                      county_s.columns]
+    county_s = county_s.iloc[:, date_cols_bool]
+    county_s = county_s.T
+    county_s.index = pd.to_datetime(county_s.index)
+    return county_s
 
 
-def data_for_table(name, ser):
-    yest = ser.iloc[-1]
-    week = ser.tail(7).sum()
-    two_week_ago = ser.tail(14).head(7).sum()
-    if two_week_ago > 0:
-        week_change = (week / two_week_ago - 1) * 100
-    else:
-        week_change = 100
-    week_change = int(round(week_change))
-    return name, yest, week, two_week_ago, str(week_change) + '%'
+def county_data(county_key):
+    print('Reading CSV')
+    cases = pd.read_csv(CASES_FILE)
+    deaths = pd.read_csv(DEATHS_FILE)
 
-
-def preprocess_data(state='Washington', county='Whatcom'):
-    cases = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv')
-    deaths = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv')
-
-    # cases = pd.read_csv('time_series_covid19_confirmed_US.csv')
-    # deaths = pd.read_csv('time_series_covid19_deaths_US.csv')
-
-    cases_diff = counts_by_county(cases, state).diff()[1:]
-    deaths_diff = counts_by_county(deaths, state).diff()[1:]
-
-    county_cases = cases_diff[county]
-    county_deaths = deaths_diff[county]
+    county_cases = county_series(cases, county_key).diff()[1:]
+    county_deaths = county_series(deaths, county_key).diff()[1:]
 
     df = pd.concat([county_cases, county_deaths], axis=1)
     df.columns = ['cases', 'deaths']
@@ -44,22 +40,40 @@ def preprocess_data(state='Washington', county='Whatcom'):
         df = df[1:]
 
     #TODO: Handle case where there are no positive values anywhere
-
-    df = df.clip(lower=0)
-    cases = data_for_table('Positive Tests', county_cases)
-
-    deaths = data_for_table('Deaths', county_deaths.clip(lower=0))
-
-    table_df = pd.DataFrame(data=[cases, deaths],
-                            columns=['', 'Yesterday', 'Past Week',
-                                     'Two Weeks Ago', 'Weekly Change'])
-
-    return df, table_df
+    df = df.clip(lower=0) #FIXME: Remove positive tests from previous day instead?
+    return df
 
 
-# df = preprocess_data()
-# print()
-# df.to_pickle('table_df.pkl')
+def county_summary(county_df):
+
+    def data_for_table(name, ser):
+        yest = ser.iloc[-1]
+        week = ser.tail(7).sum()
+        two_week_ago = ser.tail(14).head(7).sum()
+        if two_week_ago > 0:
+            week_change = (week / two_week_ago - 1) * 100
+        else:
+            week_change = 100
+        week_change = int(round(week_change))
+        return name, yest, week, two_week_ago, str(week_change) + '%'
+
+    cases = data_for_table('Positive Tests', county_df['cases'])
+    deaths = data_for_table('Deaths', county_df['deaths'])
+
+    summary_df = pd.DataFrame(data=[cases, deaths],
+                              columns=['', 'Yesterday', 'Past Week',
+                                       'Two Weeks Ago', 'Weekly Change'])
+
+    return summary_df
+
+
+def all_county_data(county_key):
+    county_df = county_data(county_key)
+    summary_df = county_summary(county_df)
+    return county_df, summary_df
+
+if __name__ == '__main__':
+    county_data('Bibb, Alabama, US')
 
 
 ## Ideas
