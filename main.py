6abe7da545cd7ac852e27f3e1d33a423016bdfa6
icanhazcodeplz <magnusson.brent@gmail.com>
2020-08-01 11:01:41 -0700

    Make a choropleth plot that matches nytimes hotspot map

--- main.py
+++ main.py
@@ -4,24 +4,64 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 import cufflinks as cf
+from urllib.request import urlopen
+import json
 from preprocess_data import *
 
 #FIXME: Update file locations
 CASES_FILE = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
 DEATHS_FILE = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
 
-CASES_FILE = 'time_series_covid19_confirmed_US.csv'
-DEATHS_FILE = 'time_series_covid19_deaths_US.csv'
+# CASES_FILE = 'time_series_covid19_confirmed_US.csv'
+# DEATHS_FILE = 'time_series_covid19_deaths_US.csv'
 
 print('Loading {}'.format(CASES_FILE))
-cases_df = pd.read_csv(CASES_FILE)
+cases_df = pd.read_csv(CASES_FILE).dropna()
 
 print('Loading {}'.format(DEATHS_FILE))
-deaths_df = pd.read_csv(DEATHS_FILE)
+deaths_df = pd.read_csv(DEATHS_FILE).dropna()
+
+# Convert fips to string and front fill zeros to get to 5 characters
+cases_df['FIPS'] = cases_df['FIPS'].apply(lambda n: str.zfill(str(int(n)), 5))
+deaths_df['FIPS'] = deaths_df['FIPS'].apply(lambda n: str.zfill(str(int(n)), 5))
+
+case_ave_df = cases_df.set_index('FIPS')
+case_ave_df = case_ave_df.iloc[:, -15:].T
+case_ave_df = case_ave_df.diff()[1:]
+case_ave_df = case_ave_df.clip(lower=0) #FIXME: Remove positive tests from previous day instead?
+case_ave_df = case_ave_df.rolling(7, ).mean().dropna()
+case_ave = case_ave_df.iloc[-1,:]
+case_ave.name = '7day_ave'
+
+fips_pop_df = deaths_df[['FIPS', 'Population', 'Combined_Key']].set_index('FIPS')
+fips_pop_df = pd.concat([fips_pop_df, case_ave], axis=1)
+fips_pop_df = fips_pop_df[fips_pop_df['Population'] > 0]
+
+fips_pop_df['ave_rate'] = fips_pop_df['7day_ave'] / fips_pop_df['Population'] * 100000
+fips_pop_df['ave_rate'] = round(fips_pop_df['ave_rate'], 1)
+
+case_rate_df = fips_pop_df[['Combined_Key', 'ave_rate']].reset_index()
+
+
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+
+fig_map = px.choropleth_mapbox(case_rate_df, geojson=counties, locations='FIPS', color='ave_rate',
+                               color_continuous_scale='Reds', #"YlOrRd",
+                               range_color=(0, 40),
+                               mapbox_style="carto-positron",
+                               zoom=3, center={"lat": 37.0902, "lon": -95.7129},
+                               opacity=0.5,
+                               hover_data=['Combined_Key'],
+                               labels=dict(Combined_Key='County', ave_rate='Average Daily Cases Per 100k', FIPS='id')
+                               )
+fig_map.update_layout(margin={"r":0, "t":0, "l":0, "b":0})
+# fig_map.show()
+
 
 county_keys = cases_df['Combined_Key'].unique()
 county_keys = [dict(label=k, value=k) for k in county_keys]
-
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -40,7 +80,6 @@ def county_fig(county_df, county_key):
                                  showgrid=True, ticks="outside",
                                  tickson="boundaries", ticklen=3, tickangle=45)
                       )
-    # fig.show()
     return fig
 
 
@@ -56,20 +95,21 @@ def generate_table(dataframe, max_rows=10):
 
 
 app.layout = html.Div(children=[
-    html.H2(children='Covid-19 Tracker'),
-    html.H6('Search for a County'),
-    dcc.Dropdown(
-        id='county-dropdown',
-        options=county_keys,
-        value=''
-    ),
-    html.Div(id='dd-output'),
+    html.H2(children='Covid-19 Hot Spots'),
+    # html.H6('Search for a County'),
+    # dcc.Dropdown(
+    #     id='county-dropdown',
+    #     options=county_keys,
+    #     value=''
+    # ),
+    # html.Div(id='dd-output'),
+    dcc.Graph(figure=fig_map)
 ])
 
 
-@app.callback(
-    dash.dependencies.Output('dd-output', 'children'),
-    [dash.dependencies.Input('county-dropdown', 'value')])
+# @app.callback(
+#     dash.dependencies.Output('dd-output', 'children'),
+#     [dash.dependencies.Input('county-dropdown', 'value')])
 def update_output(county_selection):
     if county_selection:
         county_df = county_data(cases_df, deaths_df, county_selection)
