3b3dd3721ffb7c64c009dd85efa2321db461cd2e
icanhazcodeplz <magnusson.brent@gmail.com>
2020-08-04 08:25:18 -0700

    Show county level data after clicking on a county on the map

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 import cufflinks as cf
@@ -15,15 +16,14 @@ DEATHS_FILE = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/
 # CASES_FILE = 'time_series_covid19_confirmed_US.csv'
 # DEATHS_FILE = 'time_series_covid19_deaths_US.csv'
 
+
 print('Loading {}'.format(CASES_FILE))
-cases_df = pd.read_csv(CASES_FILE).dropna()
-
+cases_df = pd.read_csv(CASES_FILE)
+cases_df = preprocess_raw_df(cases_df)
 print('Loading {}'.format(DEATHS_FILE))
-deaths_df = pd.read_csv(DEATHS_FILE).dropna()
+deaths_df = pd.read_csv(DEATHS_FILE)
+deaths_df = preprocess_raw_df(deaths_df)
 
-# Convert fips to string and front fill zeros to get to 5 characters
-cases_df['FIPS'] = cases_df['FIPS'].apply(lambda n: str.zfill(str(int(n)), 5))
-deaths_df['FIPS'] = deaths_df['FIPS'].apply(lambda n: str.zfill(str(int(n)), 5))
 
 case_ave_df = cases_df.set_index('FIPS')
 case_ave_df = case_ave_df.iloc[:, -15:].T
@@ -31,47 +31,44 @@ case_ave_df = case_ave_df.diff()[1:]
 case_ave_df = case_ave_df.clip(lower=0) #FIXME: Remove positive tests from previous day instead?
 case_ave_df = case_ave_df.rolling(7, ).mean().dropna()
 case_ave = case_ave_df.iloc[-1,:]
-case_ave.name = '7day_ave'
+case_ave.name = 'week_ave'
 
 fips_pop_df = deaths_df[['FIPS', 'Population', 'Combined_Key']].set_index('FIPS')
 fips_pop_df = pd.concat([fips_pop_df, case_ave], axis=1)
 fips_pop_df = fips_pop_df[fips_pop_df['Population'] > 0]
 
-fips_pop_df['ave_rate'] = fips_pop_df['7day_ave'] / fips_pop_df['Population'] * 100000
-fips_pop_df['ave_rate'] = round(fips_pop_df['ave_rate'], 1)
+fips_pop_df['ave_rate'] = fips_pop_df['week_ave'] / fips_pop_df['Population'] * 100000
+fips_county_dict = fips_pop_df['Combined_Key'].to_dict()
 
-case_rate_df = fips_pop_df[['Combined_Key', 'ave_rate']].reset_index()
+with open('geojson-counties-fips.json') as f:
+    counties = json.load(f)
 
-
-with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    counties = json.load(response)
-
-
+case_rate_df = fips_pop_df[['Combined_Key', 'ave_rate', 'week_ave']].reset_index()
 fig_map = px.choropleth_mapbox(case_rate_df, geojson=counties, locations='FIPS', color='ave_rate',
                                color_continuous_scale='Reds', #"YlOrRd",
                                range_color=(0, 40),
                                mapbox_style="carto-positron",
                                zoom=3, center={"lat": 37.0902, "lon": -95.7129},
                                opacity=0.5,
-                               hover_data=['Combined_Key'],
-                               labels=dict(Combined_Key='County', ave_rate='Average Daily Cases Per 100k', FIPS='id')
+                               hover_data=dict(FIPS=False, Combined_Key=True, ave_rate=':.1f', week_ave=':.1f'),
+                               labels=dict(Combined_Key='County',
+                                           ave_rate='Average Daily Cases Per 100k',
+                                           week_ave='Average Daily Cases')
                                )
 fig_map.update_layout(margin={"r":0, "t":0, "l":0, "b":0})
 # fig_map.show()
-
 
 county_keys = cases_df['Combined_Key'].unique()
 county_keys = [dict(label=k, value=k) for k in county_keys]
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 
 def county_fig(county_df, county_key):
     fig = county_df.iplot(asFigure=True, kind='bar', barmode='group',
-                          title='New Cases and Deaths in {}'.format(county_key))
+                          title='New Cases in {}'.format(county_key))
     fig.update_layout(autosize=False, width=650, height=350,
                       margin=dict(l=5, r=5, b=5, t=70, pad=1),
                       paper_bgcolor="LightSteelBlue",
@@ -95,72 +92,58 @@ def generate_table(dataframe, max_rows=10):
 
 
 app.layout = html.Div(children=[
-    html.H2(children='Covid-19 Hot Spots'),
+    dcc.Markdown(
+        '''
+        # Covid-19 Hot Spots
+        ##### Click on a County for more information
+        '''),
+    dcc.Graph(figure=fig_map, id='cases-map'),
+    # html.Div(className='row', children=[
+    #     html.Div([
+    #         dcc.Markdown("""
+    #                 **Click Data**
+    #
+    #                 Click on points in the graph.
+    #             """),
+    #         html.Pre(id='click-data', style=styles['pre']),
+    #         ], className='three columns'),
+    #     ]),
     # html.H6('Search for a County'),
     # dcc.Dropdown(
     #     id='county-dropdown',
     #     options=county_keys,
     #     value=''
     # ),
-    # html.Div(id='dd-output'),
-    dcc.Graph(figure=fig_map)
-])
+    html.Div(id='click-data'),
+    ])
 
 
-# @app.callback(
-#     dash.dependencies.Output('dd-output', 'children'),
-#     [dash.dependencies.Input('county-dropdown', 'value')])
-def update_output(county_selection):
-    if county_selection:
-        county_df = county_data(cases_df, deaths_df, county_selection)
-        summary_df = county_summary(county_df)
-        fig = county_fig(county_df, county_selection)
-        return html.Div(children=[generate_table(summary_df),
+@app.callback(
+    Output('click-data', 'children'),
+    [Input('cases-map', 'clickData')])
+def county_display(clickData):
+    if clickData:
+        fips = clickData['points'][0]['location']
+        county_selection = fips_county_dict[fips]
+        county_s = county_data(cases_df, county_selection)
+        if county_s is None:
+            return html.H4('No recorded positive cases in {}'.format(county_selection))
+
+        summary_df = county_summary(county_s)
+        fig = county_fig(county_s, county_selection)
+        return html.Div(children=[
+            html.H4('Data for {}'.format(county_selection)),
+            generate_table(summary_df),
             dcc.Graph(figure=fig)])
 
     return ''
 
 
+# @app.callback(
+#     Output('dd-output', 'children'),
+#     [Input('county-dropdown', 'value')])
+
+
 if __name__ == '__main__':
     app.run_server(debug=True, port=8080)
 
-#
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-#
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-#
-# colors = {
-#     'background': '#111111',
-#     'text': '#7FDBFF'
-# }
-#
-# # assumes you have a "wide-form" data frame with no index
-# # see https://plotly.com/python/wide-form/ for more options
-# df = pd.DataFrame({"x": [1, 2, 3], "SF": [4, 1, 2], "Montreal": [2, 4, 5]})
-#
-# fig = px.bar(df, x="x", y=["SF", "Montreal"], barmode="group")
-#
-# fig.update_layout(plot_bgcolor=colors['background'], paper_bgcolor=colors['background'], font_color=colors['text'])
-#
-# app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-#     html.H1(
-#         children='Hello Dash',
-#         style={
-#             'textAlign': 'center',
-#             'color': colors['text']
-#         }
-#     ),
-#
-#     html.Div(children='Dash: A web application framework for Python.', style={
-#         'textAlign': 'center',
-#         'color': colors['text']
-#     }),
-#
-#     dcc.Graph(
-#         id='example-graph-2',
-#         figure=fig
-#     )
-# ])
-#
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
--- preprocess_data.py
+++ preprocess_data.py
@@ -2,32 +2,42 @@ import pandas as pd
 import re
 
 
+def preprocess_raw_df(df):
+    df = df.dropna()
+    df = df[~(df['Admin2'] == 'Unassigned')]
+    df = df[~(df['Admin2'].str.contains('Out of'))]
+    df['Combined_Key'] = df['Combined_Key'].apply(lambda x: x.replace(', US', ''))
+
+    # Convert fips to string and front fill zeros to get to 5 characters
+    df['FIPS'] = df['FIPS'].apply(lambda n: str.zfill(str(int(n)), 5))
+    return df
+
+
 def county_series(df, county_key):
-    county_s = df[df['Combined_Key'] == county_key].set_index('Admin2')
+    county_s = df[df['Combined_Key'] == county_key]
     date_cols_bool = [bool(re.match('\d*/\d*/\d\d', c)) for c in
                       county_s.columns]
     county_s = county_s.iloc[:, date_cols_bool]
     county_s = county_s.T
     county_s.index = pd.to_datetime(county_s.index)
-    return county_s
+    county_s.columns = ['cases']
+    return county_s['cases']
 
 
-def county_data(cases_df, deaths_df, county_key):
+def county_data(cases_df, county_key):
     county_cases = county_series(cases_df, county_key).diff()[1:]
-    county_deaths = county_series(deaths_df, county_key).diff()[1:]
 
-    df = pd.concat([county_cases, county_deaths], axis=1)
-    df.columns = ['cases', 'deaths']
-
-    while (df['cases'][0] == 0.0) and (df['deaths'][0] == 0.0):
-        df = df[1:]
-
-    #TODO: Handle case where there are no positive values anywhere
-    df = df.clip(lower=0) #FIXME: Remove positive tests from previous day instead?
-    return df
+    if county_cases.sum() > 0:
+        while county_cases[0] == 0.0:
+            county_cases = county_cases[1:]
+        #FIXME: Remove positive tests from previous day instead?
+        county_cases = county_cases.clip(lower=0)
+        return county_cases
+    else:
+        return None
 
 
-def county_summary(county_df):
+def county_summary(county_s):
 
     def data_for_table(name, ser):
         yest = ser.iloc[-1]
@@ -35,18 +45,22 @@ def county_summary(county_df):
         two_week_ago = ser.tail(14).head(7).sum()
         if two_week_ago > 0:
             week_change = (week / two_week_ago - 1) * 100
+        elif (two_week_ago == 0) and (week == 0):
+            week_change = 0
         else:
             week_change = 100
         week_change = int(round(week_change))
-        return name, yest, week, two_week_ago, str(week_change) + '%'
+        if week_change >= 0:
+            week_change = '+{}%'.format(week_change)
+        else:
+            week_change = '{}%'.format(week_change)
+        return name, yest, week, two_week_ago, week_change
 
-    cases = data_for_table('Positive Tests', county_df['cases'])
-    deaths = data_for_table('Deaths', county_df['deaths'])
+    cases = data_for_table('Positive Tests', county_s)
 
-    summary_df = pd.DataFrame(data=[cases, deaths],
+    summary_df = pd.DataFrame(data=[cases],
                               columns=['', 'Yesterday', 'Past Week',
                                        'Two Weeks Ago', 'Weekly Change'])
-
     return summary_df
 
 
@@ -62,5 +76,8 @@ if __name__ == '__main__':
 # https://covid19-dashboard-online.herokuapp.com/
 # https://experience.arcgis.com/experience/a6f23959a8b14bfa989e3cda29297ded
 # https://www.esri.com/en-us/covid-19/overview#image3
+# https://graphics.reuters.com/HEALTH-CORONAVIRUS/USA-TRENDS/dgkvlgkrkpb/index.html
+# https://covidtracking.com/data#chart-annotations
+
 ## Cloud
 # https://console.cloud.google.com/freetrial/signup/tos?_ga=2.216095126.1215990170.1594321151-1114669994.1594321151&pli=1
